2eeb536ea1f8d364d336c4d749ed45d475a21569
MilkandMoe <138825643+MilkandMoe@users.noreply.github.com>
2024-02-22 19:17:17 -0500

    Add files via upload

---
+++ Leap year explorer.py
@@ -0,0 +1,24 @@
+#Tasks 1:
+    
+    
+leap_year = "2024"
+year = input("Input a given year")
+
+if leap_year == year:
+    print("Wow, is a leap year!")
+
+
+
+#Tasks 2:
+    
+leap_year = "2024"
+century_year = "1900"
+
+year = input("Input a given year")
+
+if leap_year == year:
+    print("Wow, is a leap year!")
+elif leap_year == year or (year == century_year):
+    print("Is a leap year or not!")   
+    
+#Tasks 3:
---
+++ The greatest showdown.py
@@ -0,0 +1,31 @@
+#Task 1:
+
+numbers = int(input("Enter three numbers"))
+
+if numbers < 10:
+    print("The first smallest number: " + numbers)
+elif numbers > 10:
+    print("The second middlest number: " + numbers)
+else:
+    print("The third largest number: " + numbers)
+    
+    
+
+
+#Tasks 2:
+
+numbers = input("Enter three numbers")
+
+if numbers < 10:
+    print("The first small number: " + numbers)
+elif numbers <= 5:
+    print("The second smallest number: " + numbers)
+else:
+    print("The third number: " + numbers)
+    
+    
+    
+#Tasks 3:
+    
+
+    
